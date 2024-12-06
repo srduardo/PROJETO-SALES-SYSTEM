e884efa46927f4523a2d65252871dbb31fd19f30
srduardo <eduardodev8@gmail.com>
2024-12-06 18:35:13 -0300

    Updated: SpringBootTest retirado da classe de testes

--- src/test/java/com/projeto/sistema/SistemaApplicationTests.java
+++ src/test/java/com/projeto/sistema/SistemaApplicationTests.java
@@ -3,11 +3,6 @@ package com.projeto.sistema;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SistemaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
